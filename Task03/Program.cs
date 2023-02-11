87cba345d1fe4ef92e76bd2e59e15b688774e2c9
AnnaVlady <7753277av@gmail.com>
2023-02-11 18:37:21 +0300

    added task05,task07

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -2,12 +2,12 @@
 // будет выдавать название дня недели по заданному номеру.
 // 3 -> Среда 5 -> Пятница
 Console.WriteLine("Введите номер дня недели: ");
-int numWeek = Convert.ToInt32(Console.ReadLine());
-if (numWeek == 1) Console.WriteLine("->Понедельник");
-else if (numWeek == 2) Console.WriteLine("->Вторник");
-else if (numWeek == 3) Console.WriteLine("->Среда");
-else if (numWeek == 4) Console.WriteLine("->Четверг");
-else if (numWeek == 5) Console.WriteLine("->Пятница");
-else if (numWeek == 6) Console.WriteLine("->Суббота");
-else if (numWeek == 7) Console.WriteLine("->Воскресенье");
-else Console.WriteLine("Такого дня не существует.");
+string numWeek = Console.ReadLine();
+if (numWeek == "1") Console.WriteLine("->Понедельник");
+else if (numWeek == "2") Console.WriteLine("->Вторник");
+else if (numWeek == "3") Console.WriteLine("->Среда");
+else if (numWeek == "4") Console.WriteLine("->Четверг");
+else if (numWeek == "5") Console.WriteLine("->Пятница");
+else if (numWeek == "6") Console.WriteLine("->Суббота");
+else if (numWeek == "7") Console.WriteLine("->Воскресенье");
+else Console.WriteLine("Ошибка ввода.");
