b11e664cefcbde1e4b178d40e04600f07761e343
AnnaVlady <7753277av@gmail.com>
2023-02-25 13:42:04 +0300

    added Task31-task37

---
+++ Task32/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+void InverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+        /* 
+        if(arr[i]<0)
+        {
+            arr[i] = Math.Abs(arr[i]);
+        }
+        else
+        {
+            arr[i] = -arr[i];
+        } */
+    }
+}
+int Prompt(string mess)     //метод ввода
+{
+    Console.Write(mess);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int volume = Prompt("Сколько элементов в массиве ");
+int minElem = Prompt("Минимльный элемент массива ");
+int maxElem = Prompt("Максимальный эллемент миассива ");
+int[] array = CreateArrayRndInt(volume, minElem, maxElem);
+Console.WriteLine("Исходный массив");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Массив после обработки");
+InverseArray(array);
+PrintArray(array);
+
