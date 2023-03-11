40bc37fbfa2773205b5653cb89ea2c8b26dd4427
AnnaVlady <7753277av@gmail.com>
2023-03-11 13:27:08 +0300

    added t66

--- DZ_Task66/Program.cs
+++ DZ_Task66/Program.cs
@@ -5,3 +5,19 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.WriteLine("Введите число M: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N: ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumElemsRec(number1, number2));
+
+
+int SumElemsRec(int num1, int num2)
+{
+    if (num1 == num2) return num1;
+    if (num1 > num2)
+        return num2 + SumElemsRec(num2 + 1, num1);
+    else
+        return num1 + SumElemsRec(num1 + 1, num2);
+}
